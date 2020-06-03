adff93a777ebd068205716e7e57216e64f3ea799
kate smith <kateesmith4994@gmail.com>
2020-06-03 08:54:45 +0100

    added up and down temp and power saving on and off

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,6 +8,39 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degreees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down()
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch PSM off', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch PSM back on', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,32 @@
 
 class Thermostat{
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingMode = true;
+  }
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+  up() {
+    this.temperature += 1;
+  }
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+    this.temperature -= 1;
+  }
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
   }
 };
