53343d7eeab5ef2de489b23d21d71b871a950d17
kate smith <kateesmith4994@gmail.com>
2020-06-04 07:59:56 +0100

    all buttons connected other than energy usage

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
+
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  })
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  })
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  })
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  }
 });
